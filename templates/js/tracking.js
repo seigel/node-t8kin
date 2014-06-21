9acaa80091b82b2dc149f91b0b56c5497451ff29
James Seigel <sourcetree@paddlethis.com>
2014-06-20 20:46:11 -0600

    Clean up and tracking

---
+++ templates/js/tracking.js
@@ -0,0 +1,12 @@
+mixpanel.track('pageload');
+mixpanel.track_links("nav a", "click nav link", {
+    "referrer": document.referrer
+});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-52138333-1', 't8k.in');
+ga('send', 'pageview');
